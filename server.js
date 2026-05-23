d637db610dfa3d9da1a30bc51dd83774fad12ab3
gudashi-soham <gudashisoham@gmail.com>
2026-05-23 22:08:06 +0530

    Keep server awake

--- server.js
+++ server.js
@@ -34,3 +34,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
+const https = require('https');
+setInterval(() => {
+  https.get('https://sohamtravel-backend-production.up.railway.app', () => {});
+}, 14 * 60 * 1000);
